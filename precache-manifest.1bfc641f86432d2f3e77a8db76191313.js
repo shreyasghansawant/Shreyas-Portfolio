bc9e6153df0ad18e42495d2444a58120e08269b2
Shreyas Ghansawant <shreyasghansawant@gmail.com>
2020-08-30 00:05:06 +0530

    first commit

---
+++ precache-manifest.1bfc641f86432d2f3e77a8db76191313.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55203ef144c344552861b518afc60d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70ed3eb37412594e9a7d",
+    "url": "/static/css/main.0f5bd549.chunk.css"
+  },
+  {
+    "revision": "4aa4fcde2a56d2a97706",
+    "url": "/static/js/2.fec25bb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fec25bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ed3eb37412594e9a7d",
+    "url": "/static/js/main.a98cd353.chunk.js"
+  },
+  {
+    "revision": "79c19766b9fafc23cac3",
+    "url": "/static/js/runtime-main.e387f811.js"
+  },
+  {
+    "revision": "0b815022610c589ef7a3cc2bf9bbac5a",
+    "url": "/static/media/agecal.0b815022.png"
+  },
+  {
+    "revision": "a5306d2351138a47a074f3290ca24b51",
+    "url": "/static/media/beginners_beat.a5306d23.png"
+  },
+  {
+    "revision": "56feb72ca7012357c0f6b8dad63d1383",
+    "url": "/static/media/clown2.56feb72c.jpg"
+  },
+  {
+    "revision": "e39915bcf40a046b382878c720d5acdc",
+    "url": "/static/media/fog.e39915bc.jpg"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/ig.0e84d592.png"
+  },
+  {
+    "revision": "d4666ae082d0eccca6715de01c9f7ee0",
+    "url": "/static/media/loveyourself.d4666ae0.png"
+  },
+  {
+    "revision": "f01ae640e4b7465542c7dc0546b00b85",
+    "url": "/static/media/me_joker.f01ae640.jpg"
+  },
+  {
+    "revision": "3d0e394dc12575d68a9bf4b13996e4dc",
+    "url": "/static/media/persona-chrome.3d0e394d.png"
+  },
+  {
+    "revision": "bb426c7e2e045184a69a574cc1e9b0e9",
+    "url": "/static/media/photos.bb426c7e.png"
+  },
+  {
+    "revision": "7894a7883ef71d3f766bdab8017a9ebe",
+    "url": "/static/media/shreyas.7894a788.png"
+  },
+  {
+    "revision": "893049ddebdcc54863f4372bb04ab291",
+    "url": "/static/media/shreyasghansawant.893049dd.png"
+  },
+  {
+    "revision": "7ecd360a92163c2a057d4a3103f4703b",
+    "url": "/static/media/sky.7ecd360a.svg"
+  },
+  {
+    "revision": "99d5b97885061d30008f0ad5af181b3b",
+    "url": "/static/media/webdeo.99d5b978.png"
+  }
+]);
